48bbd82460060e21e50d4162967605a1cd9eed36
Sean Wernimont <swernimo@users.noreply.github.com>
2023-12-01 13:54:17 -0700

    trying to implement getting data

--- CallerIdCoreData/Classes/CoreData_CallerId.swift
+++ CallerIdCoreData/Classes/CoreData_CallerId.swift
@@ -57,24 +57,24 @@ public class CoreData_CallerId {
 //        return container
 //    }()
     
-//    func getStoredDataFromCoreData() {
-//        let managedContext = persistentContainer.viewContext
-//        
-//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
-//        do {
-//            let result = try managedContext.fetch(fetchRequest)
-//            print("App Extension Records Found: \(result.count)")
-//            //Iterate between all the results saved into EntityName
-////            for data in result {
-////                // Do something with the stored data
-////            }
-//        } catch let error as NSError {
-//             // Replace this implementation with code to handle the error appropriately.
-//             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//             let nserror = error as NSError
-//             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//        }
-//    }
+    func getStoredDataFromCoreData() -> [CallerInfo] {
+        var callers: [CallerInfo] = []
+        do {
+            guard let context = CoreData_CallerId.shared.persistentContainer?.viewContext else { return callers}
+            let fetchRequest = NSFetchRequest<CallerInfo>(entityName: CoreData_CallerId.entityName)
+            let result = try context.fetch(fetchRequest)
+            if !result.isEmpty {
+                callers = result
+            }
+        } catch let error as NSError {
+             // Replace this implementation with code to handle the error appropriately.
+             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+             let nserror = error as NSError
+             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        return callers
+    }
 //    
 //    func saveContext() {
 //        let context = persistentContainer.viewContext
